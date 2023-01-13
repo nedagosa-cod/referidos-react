4d2e79d83cf4a228f6a2d76201ab1e2bb4e15de9
nedagosa-cod <nestorcode4004@gmail.com>
2023-01-13 05:17:39 -0600

    react

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3006;
 export const DB_HOST = process.env.DB_HOST || 'containers-us-west-118.railway.app';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '1chFuszo2jjdsFD1Jw03';
--- server/controllers/fncruds.controllers.js
+++ server/controllers/fncruds.controllers.js
@@ -1,7 +1,11 @@
 import { pool } from "../db.js"
 
 export const getRows = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM railway.users');
-    res.json(rows)
-    console.log(rows)
+    try {
+        const [rows] = await pool.query('SELECT * FROM railway.users');
+        res.json(rows)
+        console.log(rows)
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
